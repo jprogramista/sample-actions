3a78b7f1e26982b103130aacef7904011750ac22
adrian <adrian@adrians-macbook-pro.home>
2020-09-22 19:35:53 +0200

    V1

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import pkg from "./package.json";
+
+export default {
+    input: "index.js",
+    output: [
+        {
+            file: pkg.main,
+            format: "cjs",
+            exports: "named",
+            sourcemap: true
+        },
+        {
+            file: pkg.module,
+            format: "es",
+            exports: "named",
+            sourcemap: true
+        }
+    ]
+};
